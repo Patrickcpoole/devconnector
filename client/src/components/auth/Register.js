90b36ba88af3781cf8e2a1cb9f35db8a154433ba
Patrick Poole <patrickcpoole@gmail.com>
2019-08-12 14:37:49 -0600

    Added React User Authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import propTypes from 'prop-types';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 	const [formData, setFormdata] = useState({
 		name: '',
 		email: '',
@@ -26,6 +26,10 @@ const Register = ({ setAlert, register }) => {
 			register({ name, email, password });
 		}
 	};
+
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />;
+	}
 
 	return (
 		<Fragment>
@@ -86,10 +90,15 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
 	setAlert: propTypes.func.isRequired,
-	register: propTypes.func.isRequired
+	register: propTypes.func.isRequired,
+	isAuthenticated: propTypes.bool
 };
 
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-	null,
+	mapStateToProps,
 	{ setAlert, register }
 )(Register);
