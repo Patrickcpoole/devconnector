9a886e0239e82c25a30ca94e80e18baddf2db919
Patrick Poole <patrickcpoole@gmail.com>
2019-08-29 09:15:29 -0600

    Finished dev version

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profile from './components/profile/Profile';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -64,6 +66,8 @@ const App = () => {
 								path='/add-education'
 								component={AddEducation}
 							/>
+							<PrivateRoute exact path='/posts' component={Posts} />
+							<PrivateRoute exact path='/posts/:id' component={Post} />
 						</Switch>
 					</section>
 				</Fragment>
